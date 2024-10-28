185ea81ee983b5d6d9fbcb7f55bf784ec876f964
carlos_padrao <carlospadrao@baroes.com.br>
2024-10-28 19:43:56 -0300

    Create trip details Page - initial input

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -11,7 +11,7 @@ export function DestinationAndDateStep({ closeGuestsInput, openGuestsInput, isGu
     <div className='h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape'>
       <div className='flex items-center gap-2 flex-1'>
         <MapPin className="size-5 text-zinc-400" />
-        <input disabled={isGuestsInputOpen} type="text" name="" placeholder='Para onde você vai?' id=""  className='bg-transparent text-lg placeholder-zinc-400 outline-none'/>
+        <input disabled={isGuestsInputOpen} type="text" name="" placeholder='Para onde você vai?' id="" className='bg-transparent text-lg placeholder-zinc-400 outline-none'/>
       </div>
 
       <div className='flex items-center gap-2 max-w-56'>
